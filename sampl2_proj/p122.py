276ed4fb4c0f55b8dbd9f375cce43f5dca93d931
lucky319 <lakshmisunkara092@gmail.com>
2021-02-18 15:03:02 +0530

    not uploaded to git p122

---
+++ sampl2_proj/p122.py
@@ -0,0 +1,8 @@
+for i in  range(1,20):
+    print(i,end=' ')
+    if i==5:
+            print(i)
+l=[2,4,6,6,6,4,2,8,10,8]
+from collections import Counter
+print(Counter(l))
+print('hello this line added 18/2')
